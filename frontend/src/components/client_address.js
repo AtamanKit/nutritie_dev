8c6642bdfc97db8f3e865fd52f3431cc0b8cd653
AtamanKit <rodionatamaniuc@gmail.com>
2022-04-16 00:06:12 +0300

    city

--- frontend/src/components/client_address.js
+++ frontend/src/components/client_address.js
@@ -8,33 +8,36 @@ export default function ClientAddress() {
     const [lastName, setLastName] = useState('');
     const [telephone, setTelephone] = useState('');
     const [email, setEmail] = useState('');
-    const [address, setAddress] = useState('');
     const [checked, setChecked] = useState(false);
+    const [country, setCountry] = useState('Moldova');
+    const [city, setCity] = useState('');
+    const [address, setAddress] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault()
         setValidated(true)
 
-        // if (checked) {
+        if (checked) {
             fetch('http://127.0.0.1:8000/accounts/clients/', {
                 method: 'POST',
                 headers: {
-                    'Content-Typs': 'application/json',
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     first_name: firstName,
                     last_name: lastName,
                     telephone: telephone,
                     email: email,
+                    country: country,
+                    region: document.getElementById('region').value,
+                    city: city,
                     address: address,
-                    // country: document.getElementById('country').value,
-                    // region: document.getElementById('region').value,
                 })
             })
             .then(res => res.json())
             .then(result => console.log(result))
             .catch(error => console.log(error))
-        // }
+        }
         // console.log(document.getElementById('country').value);
         // console.log(document.getElementById('region').value)
     }
@@ -94,34 +97,23 @@ export default function ClientAddress() {
                     <Form.Group as={Col}>
                         <Form.Label>Email</Form.Label>
                         <Form.Control 
-                            required
+                            // required
                             type='email'
                             placeholder='Introduceci email'
                             onChange={e => setEmail(e.target.value)}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        {/* <Form.Control.Feedback type='invalid'>
                             Introduceti un email valid
-                        </Form.Control.Feedback>
+                        </Form.Control.Feedback> */}
                     </Form.Group>
                 </Row>
-                <Form.Group className='mb-3'>
-                    <Form.Label>Adresa</Form.Label>
-                    <Form.Control
-                        required
-                        placeholder='str., nr. bloc/nr. apartament (localitate rurala)'
-                        onChange={e => setAddress(e.target.value)}
-                    />
-                    <Form.Control.Feedback type='invalid'>
-                        Introduceti adresa
-                    </Form.Control.Feedback>
-                </Form.Group>
                 <Row className='mb-3'>
                     <Form.Group as={Col}>
                         <Form.Label>Tara</Form.Label>
                         <Form.Select 
                             required
                             defaultValue='Alegeti...'
-                            id='country'
+                            onChange={e => setCountry(e.target.value)}
                         >
                             <option value='Moldova'>Republica Moldova</option>
                             <option value='Romania'>Romania</option>
@@ -132,47 +124,120 @@ export default function ClientAddress() {
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Raion/Judet</Form.Label>
-                        <Form.Select 
-                            required
-                            defaultValue='Alegeti...'
-                            id='region'
-                        >
-                            <option value='Anenii Noi'>Anenii Noi</option>
-                            <option value='Basarabeasca'>Basarabeasca</option>
-                            <option value='Briceni'>Briceni</option>
-                            <option value='Cahul'>Cahul</option>
-                            <option value='Cantemir'>Cantemir</option>
-                            <option value='Calarasi'>Calarasi</option>
-                            <option value='Causeni'>Causeni</option>
-                            <option value='Cimislia'>Cimislia</option>
-                            <option value='Criuleni'>Criuleni</option>
-                            <option value='Donduseni'>Donduseni</option>
-                            <option value='Drochia'>Drochia</option>
-                            <option value='Dubasari'>Dubasari</option>
-                            <option value='Edinet'>Edinet</option>
-                            <option value='Falesti'>Falesti</option>
-                            <option value='Floresti'>Floresti</option>
-                            <option value='Glodeni'>Glodeni</option>
-                            <option value='Hincesti'>Hincesti</option>
-                            <option value='Ialoveni'>Ialoveni</option>
-                            <option value='Leova'>Leova</option>
-                            <option value='Nisporeni'>Nisporeni</option>
-                            <option value='Ocnita'>Ocnita</option>
-                            <option value='Orhei'>Orhei</option>
-                            <option value='Rezina'>Rezina</option>
-                            <option value='Riscani'>Riscani</option>
-                            <option value='Singerei'>Singerei</option>
-                            <option value='Soroca'>Soroca</option>
-                            <option value='Straseni'>Straseni</option>
-                            <option value='Soldanesti'>Soldanesti</option>
-                            <option value='Stefan Voda'>Stefan Voda</option>
-                            <option value='Taraclia'>Taraclia</option>
-                            <option value='Telenesti'>Telenesti</option>
-                            <option value='Ungheni'>Ungheni</option>
-                        </Form.Select>
+                        {
+                            country === 'Moldova'
+                            ?   <Form.Select 
+                                    required
+                                    defaultValue='Alegeti...'
+                                    id='region'
+                                >   
+                                    <option value='Anenii Noi'>Anenii Noi</option>
+                                    <option value='Basarabeasca'>Basarabeasca</option>
+                                    <option value='Briceni'>Briceni</option>
+                                    <option value='Cahul'>Cahul</option>
+                                    <option value='Cantemir'>Cantemir</option>
+                                    <option value='Calarasi'>Calarasi</option>
+                                    <option value='Causeni'>Causeni</option>
+                                    <option value='Cimislia'>Cimislia</option>
+                                    <option value='Criuleni'>Criuleni</option>
+                                    <option value='Donduseni'>Donduseni</option>
+                                    <option value='Drochia'>Drochia</option>
+                                    <option value='Dubasari'>Dubasari</option>
+                                    <option value='Edinet'>Edinet</option>
+                                    <option value='Falesti'>Falesti</option>
+                                    <option value='Floresti'>Floresti</option>
+                                    <option value='Glodeni'>Glodeni</option>
+                                    <option value='Hincesti'>Hincesti</option>
+                                    <option value='Ialoveni'>Ialoveni</option>
+                                    <option value='Leova'>Leova</option>
+                                    <option value='Nisporeni'>Nisporeni</option>
+                                    <option value='Ocnita'>Ocnita</option>
+                                    <option value='Orhei'>Orhei</option>
+                                    <option value='Rezina'>Rezina</option>
+                                    <option value='Riscani'>Riscani</option>
+                                    <option value='Singerei'>Singerei</option>
+                                    <option value='Soroca'>Soroca</option>
+                                    <option value='Straseni'>Straseni</option>
+                                    <option value='Soldanesti'>Soldanesti</option>
+                                    <option value='Stefan Voda'>Stefan Voda</option>
+                                    <option value='Taraclia'>Taraclia</option>
+                                    <option value='Telenesti'>Telenesti</option>
+                                    <option value='Ungheni'>Ungheni</option>
+                                </Form.Select>
+                            :   <Form.Select
+                                    required
+                                    defaultValue='Alegeti...'
+                                    id='region'
+                                >
+                                    <option value='Alba'>Alba</option>
+                                    <option value='Arad'>Arad</option>
+                                    <option value='Arges'>Arges</option>
+                                    <option value='Bacau'>Bacau</option>
+                                    <option value='Bihor'>Bihor</option>
+                                    <option value='Bistrita-Nasaud'>Bistrita-Nasaud</option>
+                                    <option value='Botosani'>Botosani</option>
+                                    <option value='Brasov'>Brasov</option>
+                                    <option value='Braila'>Braila</option>
+                                    <option value='Bucuresti'>Bucuresti</option>
+                                    <option value='Buzau'>Buzau</option>
+                                    <option value='Caras-Severin'>Caras-Severin</option>
+                                    <option value='Calarasi'>Calarasi</option>
+                                    <option value='Cluj'>Cluj</option>
+                                    <option value='Constanta'>Constanta</option>
+                                    <option value='Covasna'>Covasna</option>
+                                    <option value='Dambovita'>Dambovita</option>
+                                    <option value='Dolj'>Dolj</option>
+                                    <option value='Galati'>Galati</option>
+                                    <option value='Giurgiu'>Giurgiu</option>
+                                    <option value='Gorj'>Gorj</option>
+                                    <option value='Harghita'>Harghita</option>
+                                    <option value='Hunedoara'>Hunedoara</option>
+                                    <option value='Ialomita'>Ialomita</option>
+                                    <option value='Iasi'>Iasi</option>
+                                    <option value='Ilfov'>Ilfov</option>
+                                    <option value='Maramures'>Maramures</option>
+                                    <option value='Mehedinti'>Mehedinti</option>
+                                    <option value='Mures'>Mures</option>
+                                    <option value='Neamt'>Neamt</option>
+                                    <option value='Olt'>Olt</option>
+                                    <option value='Prahova'>Prahova</option>
+                                    <option value='Satu Mare'>Satu Mare</option>
+                                    <option value='Salaj'>Salaj</option>
+                                    <option value='Sibiu'>Sibiu</option>
+                                    <option value='Suceava'>Suceava</option>
+                                    <option value='Teleorman'>Teleorman</option>
+                                    <option value='Timis'>Timis</option>
+                                    <option value='Tulcea'>Tulcea</option>
+                                    <option value='Vaslui'>Vaslui</option>
+                                    <option value='Valcea'>Valcea</option>
+                                    <option value='Vrancea'>Vrancea</option>
+                                </Form.Select>
+                        }
+                        
+                        
                         <Form.Control.Feedback type='invalid'>
                             Introduceti regiunee
                         </Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+                <Row className='mb-3'>
+                    <Form.Group as={Col}>
+                        <Form.Label>Oras (alta localitate)</Form.Label>
+                        <Form.Control
+                            required
+                            placeholder='oras sau alta localitate'
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <Form.Control.Feedback type='invalid'>
+                            Introduceti localitatea
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Adresa</Form.Label>
+                        <Form.Control 
+                            placeholder='str., nr. bloc/nr. apartament'
+                            onChange={e => setAddress(e.target.value)}
+                        />
                     </Form.Group>
                 </Row>
 
