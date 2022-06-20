0a4aa8528d06dfdee0f54abde242fb986bc15470
AtamanKit <rodionatamaniuc@gmail.com>
2022-06-20 11:32:29 +0300

    if DEBUG=True

--- backend/nut_proj/settings.py
+++ backend/nut_proj/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
 
-if DEBUG:
+if DEBUG == True:
     ALLOWED_HOSTS = []
     CORS_ALLOWED_ORIGINS = [
         'http://localhost:3000',
